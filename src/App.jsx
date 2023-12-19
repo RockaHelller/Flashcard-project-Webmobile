93e6ae4ba1d6b9e6fdb669734c74ab4d3b6f1082
Murad Jafarov <muradjj@code.edu.az>
2023-12-19 20:35:31 +0400

    contact is partially created(design on react)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
